99017a2b9a587505de0ac4371ef3bb098f4729da
markus <marro98@gmail.com>
2021-04-27 13:32:04 +0200

    create setup.py (WIP)

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pywib',
+    version='1.0.0',
+    packages=['pywib'],
+    url='',
+    license='',
+    author='markus',
+    author_email='marro98@gmail.com',
+    description='Python Wake and Impedance Budget'
+)
