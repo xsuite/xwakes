1b1f5fd8c115c8cefb0f18976ba636918d6ab931
markus <marro98@gmail.com>
2021-05-05 17:08:41 +0200

    remove _calculate_impedance_wake

--- pywib/budget.py
+++ pywib/budget.py
@@ -11,19 +11,8 @@ class Budget:
         assert elements, "Budget object needs to be initialized with at least one Element"
         self._elements = elements
         self._model = sum(elements)
-        self._calculate_impedance_wake()
 
-    def _calculate_impedance_wake(self):
-        for c in self._model.components:
-            if c.plane == 'x':
-                self.x_impedance = lambda x: self.x_impedance(x) + c.impedance(x)
-                self.x_wake = lambda x: self.x_wake(x) + c.wake(x)
-            elif c.plane == 'y':
-                self.y_impedance = lambda x: self.y_impedance(x) + c.impedance(x)
-                self.y_wake = lambda x: self.y_wake(x) + c.wake(x)
-            elif c.plane == 'z':
-                self.z_impedance = lambda x: self.z_impedance(x) + c.impedance(x)
-                self.z_wake = lambda x: self.z_wake(x) + c.wake(x)
+
 
     @property
     def elements(self):
@@ -33,7 +22,6 @@ class Budget:
     def elements(self, elements: List[Element]):
         self._elements = elements
         self._model = sum(elements)
-        self._calculate_impedance_wake()
 
     @property
     def model(self):
