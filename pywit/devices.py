dcfcb71fb3bbfe2b4ad2a12f9e2c58365f0768f5
lgiacome <lorenzo.giacome@cern.ch>
2023-05-15 21:50:42 +0200

    refactor: implement corrections from review

--- pywit/devices.py
+++ pywit/devices.py
@@ -1,8 +1,12 @@
-from typing import Tuple, Sequence, Union
+from typing import Tuple
 from pywit.component import Component
+from pywit.element import Element
 from pywit.utilities import create_resonator_component
+from pywit.interface import component_names
+from pywit.utils import create_list
 
 import numpy as np
+from scipy import integrate
 
 from scipy.constants import c as c_light, mu_0
 from scipy.special import erfc, zeta
@@ -65,8 +69,8 @@ def create_tesla_cavity_component(plane: str, exponents: Tuple[int, int, int, in
         impedance = transverse_dipolar_impedance_tesla_cavity
         wake = transverse_dipolar_wake_tesla_cavity
     else:
-        print("Warning: resistive wall impedance not implemented for component {}{}. Set to zero".format(plane,
-                                                                                                         exponents))
+        print("Warning: tesla cavity impedance not implemented for component {}{}. Set to zero".format(plane,
+                                                                                                       exponents))
 
         def zero_function(x):
             return np.zeros_like(x)
@@ -79,8 +83,91 @@ def create_tesla_cavity_component(plane: str, exponents: Tuple[int, int, int, in
                      plane=plane, source_exponents=exponents[:2], test_exponents=exponents[2:])
 
 
+def g_addend(m, x, g_index):
+    """
+    Computes the m-th addend of series defining F (when Gint=0) or G_[Gint] function from Stupakov's formulas for a
+    rectangular linear taper at a given halfagp/width. m can be an array, in which case the function returns an array.
+    See Phys. Rev. STAB 10, 094401 (2007)
+    :param m: the index of the addend (it can be an array)
+    :param x: halfagp/width ratio
+    :param g_index: the index of the G function
+    """
+
+    if g_index == 0:
+        val = (2 * m + 1) * np.pi * x / 2
+        res2 = ((1 - np.exp(-2 * val)) / (1 + np.exp(-2 * val)) * (
+                2 * (np.exp(-val)) / (1 + np.exp(-2 * val))) ** 2) / (2 * m + 1)
+
+        return res2
+
+    elif g_index == 1:
+        val = (2 * m + 1) * np.pi * x / 2
+        res2 = x ** 3 * (2 * m + 1) * (4 * np.exp(-2 * val)) * (1 + np.exp(-2 * val)) / ((1 - np.exp(-2 * val)) ** 3)
+        return res2
+
+    elif g_index == 2:
+        val = (2 * m + 1) * np.pi * x / 2
+        res2 = x ** 2 * (2 * m + 1) * (
+            ((1 - np.exp(-2 * val)) / (1 + np.exp(-2 * val)) * (2 * (np.exp(-val)) / (1 + np.exp(-2 * val))) ** 2))
+        return res2
+
+    elif g_index == 3:
+        val = m * np.pi * x
+        res2 = x ** 2 * (2 * m) * ((1 - np.exp(-2 * val)) / (1 + np.exp(-2 * val)) * (
+                2 * (np.exp(-val)) / (1 + np.exp(-2 * val))) ** 2)
+        return res2
+
+    else:
+        print("Pb in G_element for Stupakov's formulas: Gint not 0, 1, 2 or 3 !")
+
+
+def g_stupakov(x: float, g_index: int):
+    """
+    Computes F (when Gint=0) or G_[Gint] function from Stupakov's formulas for a rectangular linear taper at a given
+    x=halfagp/width ratio. See Phys. Rev. STAB 10, 094401 (2007)
+    :param x: the halfagp/width ratio
+    :param g_index: the index of the G function
+    """
+    x = np.array(create_list(x))
+    g = np.zeros(len(x))
+
+    for ix, xelem in enumerate(x):
+
+        old_g = 1.
+        eps = 1e-5  # relative precision of the summation
+        incr = 10  # increment for sum computation
+        m = np.arange(incr)
+        m_limit = 1e6
+
+        while (abs(g[ix] - old_g) > eps * abs(g[ix])) and (m[-1] < m_limit):
+            g_array = g_addend(m, xelem, g_index)
+            old_g = g[ix]
+            g[ix] += np.sum(g_array)
+            m += incr
+
+        if m[-1] >= m_limit:
+            print("Warning: maximum number of elements reached in g_stupakov!", m[-1], xelem, ", err=",
+                  abs((g[ix] - old_g) / g[ix]))
+
+    return g
+
+
+def integrand_stupakov(g: float, w: float, g_index: int, g_power: int):
+    """
+    Computes the integrand for the Stupakov integral for a rectangular linear taper at a given vertical half-gap g
+    Note that (g')^2 has been taken out of the integral. See Phys. Rev. STAB 10, 094401 (2007)
+    :param g: the small halfagp of the taper
+    :param w: the width of the taper
+    :param g_index: the indice of the G function to use (0 is for G0=F in Stupakov's paper)
+    :param g_power: is the power to which 1/g is taken
+    """
+
+    return g_stupakov(g / w, g_index) / (g ** g_power)
+
+
 def shunt_impedance_flat_taper_stupakov_formula(half_gap_small: float, half_gap_big: float, taper_slope: float,
-                                                half_width: float, cutoff_frequency: float, comp: str=None):
+                                                half_width: float, cutoff_frequency: float = None,
+                                                component_id: str = None, approximate_integrals: bool = False) -> float:
     """
     Computes the shunt impedance in Ohm(/m if not longitudinal) of one single rectangular linear taper using Stupakov's
     formulae (Phys. Rev. STAB 10, 094401 - 2007), multiplied by Z0*c/(4*pi) to convert to SI units.
@@ -96,75 +183,122 @@ def shunt_impedance_flat_taper_stupakov_formula(half_gap_small: float, half_gap_
     :param taper_slope: the slope of the taper
     :param half_width: width of the taper (constant)
     :param cutoff_frequency: the cutoff frequency (used only for the longitudinal component)
-    :param comp: is a list with the names or a single name of the components for which ones computes the R/Q
-    (ex: Zlong, Zydip, Zxquad, etc.)
-
+    :param component_id: is a list with the names or a single name of the components for which ones computes the R/Q
+    (ex: zlong, zydip, zxquad, etc.)
+    :param approximate_integrals: use approximated formulas to compute the integrals. It can be used if one assumes
+    small half_gap_big/half_width ratio
     :return: the shunt impedance of each component in comp
     """
-    if comp is None:
-        comp = ['zlong', 'zxdip', 'zydip', 'zxqua', 'zyqua']
-
-    scalar_input = False
-    if np.isscalar(comp):
-        scalar_input = True
-        comp = [comp]
-
     z_0 = mu_0 * c_light  # free space impedance
-    r_shunt = []
 
-    for comp_id in comp:
+    if component_id == 'zlong':
+        g_index = 0
+        g_power = 0
+        cst = 4. * mu_0 * cutoff_frequency / 2.  # factor 4 due to use of half-gaps here
+        i = 7. * zeta(3, 1) / (2. * np.pi ** 2) * taper_slope * (
+                half_gap_big - half_gap_small)  # approx. integral (sp.zeta(3.,1.) is Riemann zeta function at x=3)
+        # I=7.*1.202057/(2.*np.pi**2)*tantheta*(b-a);
 
-        if comp_id == 'zlong':
-            cst = 4. * mu_0 * cutoff_frequency / 2.  # factor 4 due to use of half-gaps here
-            I = 7. * zeta(3, 1) / (2. * np.pi ** 2) * taper_slope * (
-                    half_gap_big - half_gap_small)  # approx. integral (sp.zeta(3.,1.) is Riemann zeta function at x=3)
-            # I=7.*1.202057/(2.*np.pi**2)*tantheta*(b-a);
+    elif component_id == 'zydip':
+        g_index = 1
+        g_power = 3
+        cst = z_0 * half_width * np.pi / 4.
+        i = taper_slope * (1. / (half_gap_small ** 2) - 1. / (half_gap_big ** 2)) / (
+                    2. * np.pi)  # approx. integral
 
-        elif comp_id == 'zydip':
-            cst = z_0 * half_width * np.pi / 4.
-            I = taper_slope * (1. / (half_gap_small ** 2) - 1. / (half_gap_big ** 2)) / (
-                        2. * np.pi)  # approx. integral
+    elif component_id == 'zxqua':
+        g_index = 2
+        g_power = 2
+        cst = -z_0 * np.pi / 4.
+        i = taper_slope * (1. / half_gap_small - 1. / half_gap_big) / (np.pi ** 2)  # approx. integral
 
-        elif comp_id == 'zxqua':
-            cst = -z_0 * np.pi / 4.
-            I = taper_slope * (1. / half_gap_small - 1. / half_gap_big) / (np.pi ** 2)  # approx. integral
+    elif component_id == 'zxdip':
+        g_index = 3
+        g_power = 2
+        cst = z_0 * np.pi / 4.
+        i = taper_slope * (1. / half_gap_small - 1. / half_gap_big) / (np.pi ** 2)  # approx. integral
 
-        elif comp_id == 'zxdip' or comp_id == 'zyqua':
-            cst = z_0 * np.pi / 4.
-            I = taper_slope * (1. / half_gap_small - 1. / half_gap_big) / (np.pi ** 2)  # approx. integral
-        else:
-            # mock backup values
-            cst = 0
-            I = 0
-
-        # shunt impedance /Q
-        r_shunt.append(cst * I)
-
-    if scalar_input:
-        return r_shunt[0]
+    elif component_id == 'zyqua':
+        g_index = 2
+        g_power = 2
+        cst = z_0 * np.pi / 4.
+        i = taper_slope * (1. / half_gap_small - 1. / half_gap_big) / (np.pi ** 2)  # approx. integral
     else:
-        return np.array(r_shunt)
+        # mock backup values
+        g_power = 0
+        g_index = 0
+        cst = 0
+        i = 0
+
+    if not approximate_integrals:
+        # computes numerically the integral instead of using its approximation
+        i, err = integrate.quadrature(integrand_stupakov, half_gap_small, half_gap_big,
+                                      args=(half_width, g_index, g_power), tol=1.e-3, maxiter=200)
+        i *= taper_slope  # put back g' factor that was dropped
+
+    return cst * i
 
 
-def create_flat_taper_stupakov_formula_component(plane: str, exponents: Tuple[int, int, int, int],
-                                                 half_gap_small: float, half_gap_big: float, taper_slope: float,
-                                                 half_width: float, cutoff_frequency: float, comp: str=None):
+def create_flat_taper_stupakov_formula_component(half_gap_small: float, half_gap_big: float, taper_slope: float,
+                                                 half_width: float, plane: str, exponents: Tuple[int, int, int, int],
+                                                 cutoff_frequency: float = None, component_id: str = None) -> Component:
     """
     Creates a component using the flat taper Stupakov formula
-    :param plane: the plane the component corresponds to
-    :param exponents: four integers corresponding to (source_x, source_y, test_x, test_y) aka (a, b, c, d)
     :param half_gap_small: small vertical half-gap
     :param half_gap_big: large vertical half-gap
     :param taper_slope: the slope of the taper
     :param half_width: width of the taper (constant)
+    :param plane: the plane the component corresponds to
+    :param exponents: four integers corresponding to (source_x, source_y, test_x, test_y) aka (a, b, c, d)
     :param cutoff_frequency: the cutoff frequency (used only for the longitudinal component)
-    :param comp: is a list with the names or a single name of the components for which ones computes the R/Q
-    (ex: Zlong, Zydip, Zxquad, etc.)
+    :param component_id: the components for which the R/Q is computed (ex: zlong, zydip, zxqua, etc.)
     :return: A component object of a flat taper
     """
+    if component_id not in component_names.keys():
+        raise ValueError(f"component_id must be one of the following: {component_names.keys()}")
+
     r_shunt = shunt_impedance_flat_taper_stupakov_formula(half_gap_small=half_gap_small, half_gap_big=half_gap_big,
-                                                          taper_slope=taper_slope,
-                                                          half_width=half_width, cutoff_frequency=cutoff_frequency,
-                                                          comp=comp)
+                                                          taper_slope=taper_slope, half_width=half_width,
+                                                          cutoff_frequency=cutoff_frequency, component_id=component_id)
 
     return create_resonator_component(plane=plane, exponents=exponents, r=r_shunt, q=1, f_r=cutoff_frequency)
+
+
+def create_flat_taper_stupakov_formula_element(half_gap_small: float, half_gap_big: float, taper_slope: float,
+                                               half_width: float, beta_x: float, beta_y: float,
+                                               cutoff_frequency: float = None, component_ids: str = None,
+                                               name: str = "Flat taper", tag: str = "",
+                                               description: str = "") -> Element:
+    """
+    Creates a component using the flat taper Stupakov formula
+    :param half_gap_small: small vertical half-gap
+    :param half_gap_big: large vertical half-gap
+    :param taper_slope: the slope of the taper
+    :param half_width: width of the taper (constant)
+    :param beta_x: The size of the beta function in the x-plane at the position of the taper
+    :param beta_y: The size of the beta function in the y-plane at the position of the taper
+    :param cutoff_frequency: the cutoff frequency (used only for the longitudinal component)
+    :param component_ids: a list of components to be computed
+    :param name: A user-specified name of the Element
+    :param tag: A string corresponding to a specific Element
+    :param description: A description for the Element
+    :return: A component object of a flat taper
+    """
+    length = (half_gap_big - half_gap_small) * taper_slope
+
+    if component_ids is None:
+        component_ids = ['zlong', 'zxdip', 'zydip', 'zxqua', 'zyqua']
+
+    components = []
+    for component_id in component_ids:
+        plane, exponents, _ = component_names[component_id]
+        components.append(create_flat_taper_stupakov_formula_component(half_gap_small=half_gap_small,
+                                                                       half_gap_big=half_gap_big,
+                                                                       taper_slope=taper_slope,
+                                                                       half_width=half_width,
+                                                                       plane=plane, exponents=exponents,
+                                                                       cutoff_frequency=cutoff_frequency,
+                                                                       component_id=component_id))
+
+    return Element(length=length, beta_x=beta_x, beta_y=beta_y, components=components, name=name, tag=tag,
+                   description=description)
--- pywit/utils.py
+++ pywit/utils.py
@@ -1,4 +1,5 @@
 from typing import Sequence, Union
+from copy import deepcopy
 
 import numpy as np
 
@@ -17,3 +18,17 @@ def round_sigfigs(arr: Union[float,Sequence[float]], sigfigs: int):
         return np.array([round_sigfigs(value, sigfigs) for value in arr])
 
 
+def create_list(a, n=1):
+    """
+    if a is a scalar, return a list containing n times the element a
+    otherwise return a
+    """
+    if not (np.isscalar(a)):
+        pass
+    else:
+        b = []
+        for i in range(n):
+            b.append(a)
+        a = deepcopy(b)
+
+    return a
