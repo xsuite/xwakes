46828ee2b7e642658c1c912820a3ed352aebe5c6
Lorenzo Giacomel <lorenzo.giacomel@cern.ch>
2023-05-17 14:15:10 +0200

    test: use a brodband resonator in sacherer test

--- test/test_sacherer.py
+++ test/test_sacherer.py
@@ -3,144 +3,109 @@ import numpy as np
 from scipy import constants as cst
 
 from pywit.sacherer_formula import sacherer_formula
+from pywit.utilities import create_resonator_component
+
+tune_shift_nx_ref = np.array([[-1.89467139e-03 + 2.00708263e-06j, -3.59042292e-05 + 4.25385266e-05j,
+                               1.83556168e-03 + 2.00708264e-06j],
+                              [-1.89467139e-03 + 2.00708463e-06j, -3.59042230e-05 + 4.25385269e-05j,
+                               1.83556167e-03 + 2.00708463e-06j],
+                              [-1.89467139e-03 + 2.00708662e-06j, -3.59042167e-05 + 4.25385272e-05j,
+                               1.83556167e-03 + 2.00708663e-06j],
+                              [-1.89467139e-03 + 2.00708863e-06j, -3.59042104e-05 + 4.25385275e-05j,
+                               1.83556167e-03 + 2.00708863e-06j],
+                              [-1.89467140e-03 + 2.00709063e-06j, -3.59042042e-05 + 4.25385278e-05j,
+                               1.83556167e-03 + 2.00709064e-06j]])
+
+tune_shift_m0_ref = (-3.5904229237583215e-05 + 4.253852659650811e-05j)
+
+z_eff_ref = np.array([[673415.90132594 + 9916238.18255225j,
+                       7136258.30545439 + 6023289.34727782j,
+                       673415.90381543 + 9916238.18553716j],
+                      [673416.56964372 + 9916238.98384426j,
+                       7136258.35621533 + 6023288.2941135j,
+                       673416.57214035 + 9916238.98683727j],
+                      [673417.23974633 + 9916239.78730782j,
+                       7136258.40689439 + 6023287.24166201j,
+                       673417.24224903 + 9916239.79030961j],
+                      [673417.91165383 + 9916240.59313587j,
+                       7136258.45756425 + 6023286.19008073j,
+                       673417.91416421 + 9916240.59614618j],
+                      [673418.58579267 + 9916241.40106015j,
+                       7136258.50840891 + 6023285.1389662j,
+                       673418.58831139 + 9916241.40407765j]])
 
 
-def test_sacherer_formula_table():
-    impedance_data = np.loadtxt('test/test_data/lhc_flattop_impedance/Zx_dip_reference.dat', skiprows=1, dtype=float)
+def test_sacherer_formula_function():
+    impedance_func = create_resonator_component(plane='x', exponents=(1, 0, 0, 0), f_r=0.5e9, q=1, r=1e7).impedance
+
+    def impedance_func_correct(x):
+        if np.isscalar(x):
+            x = np.array([x])
+        ind_p = x >= 0
+        ind_n = x < 0
+        result = np.zeros_like(x, dtype=complex)
+        if len(x[ind_p]) > 0:
+            result[ind_p] = impedance_func(x[ind_p])
+        if len(x[ind_n]) > 0:
+            result[ind_n] = -impedance_func(np.abs(x[ind_n])).conjugate()
+
+        return result
 
     bunch_intensity = 1.4E11
-    bunch_length = 1.1E-9
+    bunch_length = 3E-9
     omega_rev = 2.0 * np.pi * cst.c / 26658.8832
     tune = 62.28
     gamma = 6.8E6 / cst.value('proton mass energy equivalent in MeV')
     eta = 0.0003475406157 - 1 / gamma ** 2
-    V = 12E6
+    rf_cavities_voltage = 12E6
     beta = np.sqrt(1. - 1. / (gamma ** 2))
     p0 = cst.m_p * beta * gamma * cst.c
     h = 35640
-    q_s = np.sqrt(cst.e * V * eta * h / (2 * np.pi * beta * cst.c * p0))
+    q_s = np.sqrt(cst.e * rf_cavities_voltage * eta * h / (2 * np.pi * beta * cst.c * p0))
     m_max = 1
     n_bunches = 5
     nx_array = np.linspace(0, n_bunches - 1, n_bunches, dtype=int)
-    complex_data = impedance_data[:, 1] + 1j * impedance_data[:, 2]
 
     tune_shift_nx, tune_shift_m0, z_eff = sacherer_formula(qp=10, nx_array=nx_array,
                                                            bunch_intensity=bunch_intensity,
                                                            omegas=q_s * omega_rev, n_bunches=n_bunches,
                                                            omega_rev=omega_rev, tune=tune, gamma=gamma, eta=eta,
                                                            bunch_length_seconds=bunch_length,
-                                                           m_max=m_max, impedance_table=complex_data,
-                                                           freq_impedance_table=impedance_data[:, 0],
-                                                           mode_type='sinusoidal', flag_trapz=None)
-
-    tune_shift_nx_ref = np.array([[-0.00201982 - 1.17593889e-05j,
-                                   -0.00040042 + 8.92895664e-05j,
-                                   0.00171041 - 1.17597337e-05j],
-                                  [-0.00201981 - 1.17806341e-05j,
-                                   -0.00040036 + 8.91650444e-05j,
-                                   0.00171042 - 1.17806668e-05j],
-                                  [-0.00201981 - 1.17878214e-05j,
-                                   -0.00040035 + 8.91226650e-05j,
-                                   0.00171043 - 1.17878453e-05j],
-                                  [-0.00201981 - 1.17943871e-05j,
-                                   -0.00040035 + 8.90838416e-05j,
-                                   0.00171043 - 1.17944148e-05j],
-                                  [-0.00201981 - 1.18039769e-05j,
-                                   -0.00040038 + 8.90270672e-05j,
-                                   0.00171042 - 1.18040287e-05j]])
-
-    tune_shift_m0_ref = (-0.0004003813037264127 + 8.902706715083235e-05j)
-    z_eff_ref = np.array([[-1446686.05890183 + 19032104.51982384j,
-                           5492376.02563156 + 24630660.07462981j,
-                           -1446728.4784696 + 19032089.95301624j],
-                          [-1449299.72813343 + 19030859.78223774j,
-                           5484716.43386444 + 24627004.36953009j,
-                           -1449303.75267617 + 19030857.99329085j],
-                          [-1450183.93835416 + 19030596.99730265j,
-                           5482109.59394675 + 24626231.77510942j,
-                           -1450186.88299486 + 19030596.9306514j],
-                          [-1450991.6851084 + 19030710.94121455j,
-                           5479721.49042725 + 24626565.66937676j,
-                           -1450995.08259959 + 19030711.94470993j],
-                          [-1452171.45336082 + 19031275.98435284j,
-                           5476229.18178966 + 24628237.78771698j,
-                           -1452177.83002988 + 19031279.61551182j]])
+                                                           m_max=m_max, impedance_function=impedance_func_correct,
+                                                           mode_type='sinusoidal', flag_trapz=False)
 
     assert np.allclose(tune_shift_nx, tune_shift_nx_ref)
     assert np.isclose(tune_shift_m0, tune_shift_m0_ref)
     assert np.allclose(z_eff, z_eff_ref)
 
 
-def test_sacherer_formula_function():
-    impedance_data = np.loadtxt('test/test_data/lhc_flattop_impedance/Zx_dip_reference.dat', skiprows=1, dtype=float)
-
+def test_sacherer_formula_table():
+    resonator_component = create_resonator_component(plane='x', exponents=(1, 0, 0, 0), f_r=0.5e9, q=1, r=1e7)
+    freq_impedance_table, impedance_table = resonator_component.impedance_to_array(10 ** 5, 1e-8, 1e15)
+    # breakpoint()
     bunch_intensity = 1.4E11
-    bunch_length = 1.1E-9
+    bunch_length = 3E-9
     omega_rev = 2.0 * np.pi * cst.c / 26658.8832
     tune = 62.28
     gamma = 6.8E6 / cst.value('proton mass energy equivalent in MeV')
     eta = 0.0003475406157 - 1 / gamma ** 2
-    V = 12E6
+    rf_cavities_voltage = 12E6
     beta = np.sqrt(1. - 1. / (gamma ** 2))
     p0 = cst.m_p * beta * gamma * cst.c
     h = 35640
-    q_s = np.sqrt(cst.e * V * eta * h / (2 * np.pi * beta * cst.c * p0))
+    q_s = np.sqrt(cst.e * rf_cavities_voltage * eta * h / (2 * np.pi * beta * cst.c * p0))
     m_max = 1
     n_bunches = 5
-    nx_array = np.linspace(0, n_bunches - 1, n_bunches)
-    complex_data = impedance_data[:, 1] + 1j * impedance_data[:, 2]
-
-    def impedance_function(x):
-        if np.isscalar(x):
-            x = np.array([x])
-        ind_p = x >= 0
-        ind_n = x < 0
-        result = np.zeros_like(x, dtype=complex)
-        result[ind_p] = numpy.interp(x[ind_p], impedance_data[:, 0], complex_data)
-        result[ind_n] = -numpy.interp(np.abs(x[ind_n]), impedance_data[:, 0], complex_data).conjugate()
-
-        return result
+    nx_array = np.linspace(0, n_bunches - 1, n_bunches, dtype=int)
 
     tune_shift_nx, tune_shift_m0, z_eff = sacherer_formula(qp=10, nx_array=nx_array,
                                                            bunch_intensity=bunch_intensity,
                                                            omegas=q_s * omega_rev, n_bunches=n_bunches,
                                                            omega_rev=omega_rev, tune=tune, gamma=gamma, eta=eta,
                                                            bunch_length_seconds=bunch_length,
-                                                           m_max=m_max, impedance_function=impedance_function,
-                                                           mode_type='sinusoidal', flag_trapz=None)
-
-    tune_shift_nx_ref = np.array([[-0.00201982 - 1.17593889e-05j,
-                                   -0.00040042 + 8.92895664e-05j,
-                                   0.00171041 - 1.17597337e-05j],
-                                  [-0.00201981 - 1.17806341e-05j,
-                                   -0.00040036 + 8.91650444e-05j,
-                                   0.00171042 - 1.17806668e-05j],
-                                  [-0.00201981 - 1.17878214e-05j,
-                                   -0.00040035 + 8.91226650e-05j,
-                                   0.00171043 - 1.17878453e-05j],
-                                  [-0.00201981 - 1.17943871e-05j,
-                                   -0.00040035 + 8.90838416e-05j,
-                                   0.00171043 - 1.17944148e-05j],
-                                  [-0.00201981 - 1.18039769e-05j,
-                                   -0.00040038 + 8.90270672e-05j,
-                                   0.00171042 - 1.18040287e-05j]])
-
-    tune_shift_m0_ref = (-0.0004003813037264127 + 8.902706715083235e-05j)
-    z_eff_ref = np.array([[-1446686.05890183 + 19032104.51982384j,
-                           5492376.02563156 + 24630660.07462981j,
-                           -1446728.4784696 + 19032089.95301624j],
-                          [-1449299.72813343 + 19030859.78223774j,
-                           5484716.43386444 + 24627004.36953009j,
-                           -1449303.75267617 + 19030857.99329085j],
-                          [-1450183.93835416 + 19030596.99730265j,
-                           5482109.59394675 + 24626231.77510942j,
-                           -1450186.88299486 + 19030596.9306514j],
-                          [-1450991.6851084 + 19030710.94121455j,
-                           5479721.49042725 + 24626565.66937676j,
-                           -1450995.08259959 + 19030711.94470993j],
-                          [-1452171.45336082 + 19031275.98435284j,
-                           5476229.18178966 + 24628237.78771698j,
-                           -1452177.83002988 + 19031279.61551182j]])
+                                                           m_max=m_max, impedance_table=impedance_table,
+                                                           freq_impedance_table=freq_impedance_table,
+                                                           mode_type='sinusoidal', flag_trapz=False)
 
     assert np.allclose(tune_shift_nx, tune_shift_nx_ref)
     assert np.isclose(tune_shift_m0, tune_shift_m0_ref)
