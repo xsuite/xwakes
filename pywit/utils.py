bea2e3c0646c7a7b37543ba0aa09fc6ce18e1312
nimounet <nicolas.mounet@cern.ch>
2023-08-23 07:27:13 +0200

    Adding comments and renaming variables in pywit/utils (unique_sigfigs)

--- pywit/utils.py
+++ pywit/utils.py
@@ -8,7 +8,8 @@ def round_sigfigs(arr: Union[float, Sequence[float]], sigfigs: int):
     Rounds all the floats in an array to a the given number of
     significant digits.
     Warning: this routine is slow when working with very long
-    arrays. In this case it is better to use unique_sigfigs
+    arrays. If the final goal is to get unique elements to the given
+    precision, it is better to use unique_sigfigs.
     :param arr: A float or an array-like sequence of floats.
     :param sigfigs: The number of significant digits (integer).
     :return: The same array (or float) rounded appropriately.
@@ -21,12 +22,14 @@ def round_sigfigs(arr: Union[float, Sequence[float]], sigfigs: int):
 
 def unique_sigfigs(arr: np.ndarray, sigfigs: int):
     """
-    Given an array returns all the elements that are unique after rounding to a given number of significant digits
+    Given an array, returns it sorted and with only the elements that
+    are unique after rounding to a given number of significant digits.
     :param arr: An array of floats
     :param sigfigs: The number of significant digits (integer)
+    :return: the sorted arrays with unique elements
     """
     arr = np.unique(arr.flat)
-    i = np.argsort(arr.flat)
-    mag_d = np.floor(np.log10(np.append(np.inf, np.diff(arr[i]))))
-    mag_arr = np.floor(np.log10(arr[i]))
-    return arr.flat[i[mag_d - mag_arr >= -sigfigs]]
+    sorted_indices = np.argsort(arr.flat)
+    log_diff = np.floor(np.log10(np.append(np.inf, np.diff(arr[sorted_indices]))))
+    log_arr = np.floor(np.log10(arr[sorted_indices]))
+    return arr.flat[sorted_indices[log_diff - log_arr >= -sigfigs]]
