dfb619ab3c5dbff21c991080661b1bef4f7cb529
Lorenzo Giacomel <lorenzo.giacomel@cern.ch>
2023-09-04 08:39:56 +0200

    style: improve variable names

--- test/test_landau_damping.py
+++ test/test_landau_damping.py
@@ -10,17 +10,17 @@ def tune_shift():
 
 
 @pytest.fixture
-def b_direct_ref():
+def b_direct_expected():
     return 7.888357197059519e-05
 
 
 @pytest.fixture
-def b_cross_ref():
+def b_cross_expected():
     return -5.632222163778799e-05
 
 
 @pytest.fixture
-def i_ref():
+def i_expected():
     return 550
 
 
@@ -34,56 +34,60 @@ def polarity():
     return 1
 
 
-def test_dispersion_integral_2d(tune_shift, b_direct_ref, b_cross_ref):
+def test_dispersion_integral_2d(tune_shift, b_direct_expected, b_cross_expected):
 
     distribution = 'gaussian'
 
     # reference value obtained with DELPHI
-    ref_value = 7153.859519171599-2722.966574677259j
+    expected_value = 7153.859519171599-2722.966574677259j
 
-    assert np.isclose(np.real(ref_value), np.real(dispersion_integral_2d(tune_shift=tune_shift, b_direct=b_direct_ref,
-                                                                         b_cross=b_cross_ref,
-                                                                         distribution=distribution)))
-    assert np.isclose(np.imag(ref_value), np.imag(dispersion_integral_2d(tune_shift=tune_shift, b_direct=b_direct_ref,
-                                                                         b_cross=b_cross_ref,
-                                                                         distribution=distribution)))
+    assert np.isclose(np.real(expected_value), np.real(dispersion_integral_2d(tune_shift=tune_shift,
+                                                                              b_direct=b_direct_expected,
+                                                                              b_cross=b_cross_expected,
+                                                                              distribution=distribution)))
+    assert np.isclose(np.imag(expected_value), np.imag(dispersion_integral_2d(tune_shift=tune_shift,
+                                                                              b_direct=b_direct_expected,
+                                                                              b_cross=b_cross_expected,
+                                                                              distribution=distribution)))
 
     distribution = 'parabolic'
 
     # reference value obtained with DELPHI
-    ref_value = 6649.001778623455-3168.4257879737443j
+    expected_value = 6649.001778623455-3168.4257879737443j
 
-    assert np.isclose(np.real(ref_value), np.real(dispersion_integral_2d(tune_shift=tune_shift, b_direct=b_direct_ref,
-                                                                         b_cross=b_cross_ref,
-                                                                         distribution=distribution)))
-    assert np.isclose(np.imag(ref_value), np.imag(dispersion_integral_2d(tune_shift=tune_shift, b_direct=b_direct_ref,
-                                                                         b_cross=b_cross_ref,
-                                                                         distribution=distribution)))
+    assert np.isclose(np.real(expected_value), np.real(dispersion_integral_2d(tune_shift=tune_shift,
+                                                                              b_direct=b_direct_expected,
+                                                                              b_cross=b_cross_expected,
+                                                                              distribution=distribution)))
+    assert np.isclose(np.imag(expected_value), np.imag(dispersion_integral_2d(tune_shift=tune_shift,
+                                                                              b_direct=b_direct_expected,
+                                                                              b_cross=b_cross_expected,
+                                                                              distribution=distribution)))
 
 
-def test_find_octupole_threshold(tune_shift, b_direct_ref, b_cross_ref, i_ref, q_s):
+def test_find_octupole_threshold(tune_shift, b_direct_expected, b_cross_expected, i_expected, q_s):
     # reference value obtained with the old impedance wake model https://gitlab.cern.ch/IRIS/HLLHC_IW_model
     b_direct_expected = 1.980315192200037e-05
     b_cross_expected = -1.4139287608495406e-05
     assert np.isclose(b_direct_expected, find_octupole_threshold(tune_shift=tune_shift, q_s=q_s,
-                                                                 b_direct_ref=b_direct_ref,
-                                                                 b_cross_ref=b_cross_ref)[0])
+                                                                 b_direct_ref=b_direct_expected,
+                                                                 b_cross_ref=b_cross_expected)[0])
     assert np.isclose(b_cross_expected, find_octupole_threshold(tune_shift=tune_shift, q_s=q_s,
-                                                                b_direct_ref=b_direct_ref,
-                                                                b_cross_ref=b_cross_ref)[1])
+                                                                b_direct_ref=b_direct_expected,
+                                                                b_cross_ref=b_cross_expected)[1])
     # reference value obtained with the old impedance wake model
     b_direct_expected = -1.2718161244917965e-05
     b_cross_expected = 9.08066253298482e-06
 
     assert np.isclose(b_direct_expected, find_octupole_threshold(tune_shift=tune_shift, q_s=q_s,
-                                                                 b_direct_ref=-b_direct_ref,
-                                                                 b_cross_ref=-b_cross_ref)[0])
+                                                                 b_direct_ref=-b_direct_expected,
+                                                                 b_cross_ref=-b_cross_expected)[0])
     assert np.isclose(b_cross_expected, find_octupole_threshold(tune_shift=tune_shift, q_s=q_s,
-                                                                b_direct_ref=-b_direct_ref,
-                                                                b_cross_ref=-b_cross_ref)[1])
+                                                                b_direct_ref=-b_direct_expected,
+                                                                b_cross_ref=-b_cross_expected)[1])
 
 
-def test_find_octupole_threshold_many_tune_shifts(tune_shift, b_direct_ref, b_cross_ref, i_ref, q_s):
+def test_find_octupole_threshold_many_tune_shifts(tune_shift, b_direct_expected, b_cross_expected, i_expected, q_s):
     # reference value obtained with the old impedance wake model https://gitlab.cern.ch/IRIS/HLLHC_IW_model
     b_direct_expected = 3.960630384598084e-05
     b_cross_expected = -2.8278575218404585e-05
@@ -91,17 +95,17 @@ def test_find_octupole_threshold_many_tune_shifts(tune_shift, b_direct_ref, b_cr
     tune_shifts = [tune_shift, 2*tune_shift, np.nan]
 
     assert np.isclose(b_direct_expected, find_octupole_threshold_many_tune_shifts(tune_shifts=tune_shifts, q_s=q_s,
-                                                                                  b_direct_ref=b_direct_ref,
-                                                                                  b_cross_ref=b_cross_ref)[0])
+                                                                                  b_direct_ref=b_direct_expected,
+                                                                                  b_cross_ref=b_cross_expected)[0])
     assert np.isclose(b_cross_expected, find_octupole_threshold_many_tune_shifts(tune_shifts=tune_shifts, q_s=q_s,
-                                                                                 b_direct_ref=b_direct_ref,
-                                                                                 b_cross_ref=b_cross_ref)[1])
+                                                                                 b_direct_ref=b_direct_expected,
+                                                                                 b_cross_ref=b_cross_expected)[1])
     # reference value obtained with the old impedance wake model https://gitlab.cern.ch/IRIS/HLLHC_IW_model
     b_direct_expected = -2.5436322491034757e-05
     b_cross_expected = 1.816132506682559e-05
     assert np.isclose(b_direct_expected, find_octupole_threshold_many_tune_shifts(tune_shifts=tune_shifts, q_s=q_s,
-                                                                                  b_direct_ref=-b_direct_ref,
-                                                                                  b_cross_ref=-b_cross_ref)[0])
+                                                                                  b_direct_ref=-b_direct_expected,
+                                                                                  b_cross_ref=-b_cross_expected)[0])
     assert np.isclose(b_cross_expected, find_octupole_threshold_many_tune_shifts(tune_shifts=tune_shifts, q_s=q_s,
-                                                                                 b_direct_ref=-b_direct_ref,
-                                                                                 b_cross_ref=-b_cross_ref)[1])
+                                                                                 b_direct_ref=-b_direct_expected,
+                                                                                 b_cross_ref=-b_cross_expected)[1])
