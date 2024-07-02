07befeeb216ad5aa7f079b82a8d4a49cf3e03fee
giadarol <giovanni.iadarola@cern.ch>
2024-07-02 09:41:13 +0200

    First check on the sum

--- examples/002_wake_sum.py
+++ examples/002_wake_sum.py
@@ -1,20 +1,19 @@
 import xwakes as xw
 
 
-xw.WakeResonator(r=1e8, q=1e7, f_r=1e9,
+w1 = xw.WakeResonator(r=1e8, q=1e7, f_r=1e9,
     kind='dipolar_x')
 
-xw.WakeResonator(r=1e8, q=1e7, f_r=1e9,
+w2 = xw.WakeResonator(r=1e8, q=1e7, f_r=1e9,
     kind=['dipolar_x', 'dipolar_y'])
 
-xw.WakeResonator(r=1e8, q=1e7, f_r=1e9,
-    kind={'dipolar_x': 1.0, 'dipolar_y': 2.0})
-
-xw.WakeResonator(r=1e8, q=1e7, f_r=1e9,
+w3 = xw.WakeResonator(r=1e8, q=1e7, f_r=1e9,
     kind=xw.Yokoya('flat_horizontal'))
 
-xw.WakeResonator(r=1e8, q=1e7, f_r=1e9,
+w4 = xw.WakeResonator(r=1e8, q=1e7, f_r=1e9,
                  plane='y', source_exponents=(3, 0),
                  test_exponents=(0, 2))
 # is y_test^2 integral(x_source(z')^3 W(z-z') dz')
+
+w = w1 + w2 + w3 + w4
 
--- xwakes/basewake.py
+++ xwakes/basewake.py
@@ -5,15 +5,40 @@ class BaseWake:
 
     def configure_for_tracking(self, zeta_range: Tuple[float, float],
                                num_slices: int,
-                               **kwargs # for multibuunch compatibility
+                               **kwargs # for multibunch compatibility
                                ) -> None:
         import xfields as xf
-        self._xfields_wf = xf.Wakefield(components=self.components,
-                                        zeta_range=zeta_range,
-                                        num_slices=num_slices, **kwargs)
+        self._xfields_wf = xf.Wakefield(
+            components=_expand_components(self.components),
+            zeta_range=zeta_range,
+            num_slices=num_slices, **kwargs)
 
     def track(self, particles) -> None:
         self._xfields_wf.track(particles)
+
+    def __add__(self, other):
+        return _add_wakes(self, other)
+
+    def __radd__(self, other):
+        return _add_wakes(other, self)
+
+def _add_wakes(wake1, wake2):
+    out_components = []
+    for ww in [wake1, wake2]:
+        if ww is None or ww == 0:
+            continue
+        if isinstance(ww, CombinedWake):
+            out_components.extend(ww.components)
+        else:
+            out_components.append(ww)
+
+    return CombinedWake(components=out_components)
+
+
+class CombinedWake(BaseWake):
+
+    def __init__(self, components):
+        self.components = components
 
 
 def _handle_kind(kind):
@@ -26,3 +51,16 @@ def _handle_kind(kind):
     assert hasattr(kind, 'keys')
 
     return kind
+
+
+def _expand_components(components):
+    _expanded_components = []
+    for cc in components:
+        if hasattr(cc, 'function_vs_zeta'):
+            _expanded_components.append(cc)
+        else:
+            assert hasattr(cc, 'components')
+            _expanded_components.extend(
+                _expanded_components(cc.components))
+
+    return _expanded_components
