542a40e6a6d44888f0190733fd9ecb152cf9c6bd
giadarol <giovanni.iadarola@cern.ch>
2024-07-04 23:37:43 +0200

    Does not kick...

--- xwakes/__init__.py
+++ xwakes/__init__.py
@@ -4,3 +4,4 @@ from .init_pywit_directory import initialize_pywit_directory
 from .resonator import WakeResonator
 from .wakefield_from_table import WakeFromTable
 from .yokoya import Yokoya
+from .read_headtail_table import read_headtail_file
