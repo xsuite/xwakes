58a228b1ccbd1a19f9c63c7e87ca4c8cac28dcbe
giadarol <giovanni.iadarola@cern.ch>
2024-07-04 20:38:46 +0200

    OK also with beta0

--- examples/003_impedance.py
+++ examples/003_impedance.py
@@ -3,6 +3,7 @@ import xwakes as xw
 
 from scipy.constants import c as clight
 
+beta0 = 0.1
 
 res = xw.WakeResonator(r=1e8, q=10, f_r=1e9,
     kind='dipolar_x')
@@ -11,8 +12,8 @@ res = xw.WakeResonator(r=1e8, q=10, f_r=1e9,
 z = np.linspace(-20, 20, 100000)
 t = np.linspace(-20/clight, 20/clight, 100000)
 
-wx_vs_zeta = res.components[0].function_vs_zeta(z, beta0=1)
-wx_vs_t = res.components[0].function_vs_t(t, beta0=1)
+wx_vs_zeta = res.components[0].function_vs_zeta(z, beta0=beta0)
+wx_vs_t = res.components[0].function_vs_t(t, beta0=beta0)
 
 
 omega = np.linspace(-10e9, 10e9, 1000)
@@ -23,8 +24,8 @@ dz = z[1] - z[0]
 dt = t[1] - t[0]
 for ii, oo in enumerate(omega):
     print(ii, end='\r', flush=True)
-    Zx_from_zeta[ii] = 1j/clight * np.sum(wx_vs_zeta
-                * np.exp(1j * oo * z / clight)) * dz
+    Zx_from_zeta[ii] = 1j/beta0/clight * np.sum(wx_vs_zeta
+                * np.exp(1j * oo * z / beta0 / clight)) * dz
     Zx_from_t[ii] = 1j * np.sum(
                 wx_vs_t * np.exp(-1j * oo * t)) * dt
 
