e888c07a399c6f2cb9a234691ab4a1663461c32d
giadarol <giovanni.iadarola@cern.ch>
2024-07-07 15:26:39 +0200

    Works for dipola/quadrupolar x/y

--- examples/001a_tune_shift_from_transverse_wakes.py
+++ examples/001a_tune_shift_from_transverse_wakes.py
@@ -6,13 +6,13 @@ import xwakes as xw
 import xobjects as xo
 
 plane = 'y'
-wake_type = 'quadrupolar'
+wake_type = 'dipolar'
 
 table = pd.DataFrame({'time': [0, 10],
-                      f'{wake_type}_{plane}': [5e16, 5e16]})
+                      f'{wake_type}_{plane}': [6e15, 6e15]})
 
 wake = xw.WakeFromTable(table)
-wake.configure_for_tracking(zeta_range=(-10e-2, 10e-2), num_slices=1000)
+wake.configure_for_tracking(zeta_range=(-20e-2, 20e-2), num_slices=100)
 
 assert len(wake.components) == 1
 assert wake.components[0].plane == plane
@@ -23,11 +23,11 @@ assert wake.components[0].test_exponents == {
     'dipolar_x': (0, 0), 'dipolar_y': (0, 0),
     'quadrupolar_x': (1, 0), 'quadrupolar_y': (0, 1)}[f'{wake_type}_{plane}']
 
-xo.assert_allclose(wake.components[0].function_vs_t(0.1, beta0=1.), 5e16, rtol=1e-10, atol=0)
-xo.assert_allclose(wake.components[0].function_vs_t(0, beta0=1.), 5e16, rtol=1e-10, atol=0)
+xo.assert_allclose(wake.components[0].function_vs_t(0.1, beta0=1.), 6e15, rtol=1e-10, atol=0)
+xo.assert_allclose(wake.components[0].function_vs_t(0, beta0=1.), 6e15, rtol=1e-10, atol=0)
 xo.assert_allclose(wake.components[0].function_vs_t(-0.1, beta0=1.), 0, rtol=1e-10, atol=0)
 
-one_turn_map = xt.LineSegmentMap(length=1, qx=0.28, qy=0.31, qs=1e-3, bets=100)
+one_turn_map = xt.LineSegmentMap(length=1, qx=0.28, qy=0.31, qs=0.1e-3, bets=100)
 
 line_no_wake = xt.Line(elements=[one_turn_map])
 line_with_wake = xt.Line(elements=[one_turn_map, wake])
@@ -49,12 +49,12 @@ p0 = p.copy()
 mylog = xt.Log(mean_x=lambda line, part: part.x.mean(),
                mean_y=lambda line, part: part.y.mean())
 
-line_no_wake.track(p0, num_turns=1000,
+line_no_wake.track(p0, num_turns=100,
                    log=mylog,
                    with_progress=10)
 log_no_wake = line_no_wake.log_last_track
 
-line_with_wake.track(p, num_turns=1000,
+line_with_wake.track(p, num_turns=100,
                         log=mylog,
                         with_progress=10)
 log_with_wake = line_with_wake.log_last_track
@@ -66,7 +66,8 @@ tune_with_wake = nl.get_tune(log_with_wake[f'mean_{plane}'])
 
 print(f'Tune without wake: {tune_no_wake}')
 print(f'Tune with wake: {tune_with_wake}')
+print(f'Tune shift: {tune_with_wake - tune_no_wake}')
 
-# Expect negative tune shift for positive dipolar wake
+# Expect negative tune shift for positive wake
 xo.assert_allclose(tune_no_wake, {'x': 0.28, 'y': 0.31}[plane], atol=1e-6, rtol=0)
-xo.assert_allclose(tune_with_wake, tune_no_wake - 6e-3, atol=1.5e-3, rtol=0)
+xo.assert_allclose(tune_with_wake, tune_no_wake - 2e-3, atol=0.3e-3, rtol=0)
