46ae2e70e41a8ce4fef641bc8755ab528de2ca48
giadarol <giovanni.iadarola@cern.ch>
2024-07-10 23:34:02 +0200

    Add tests

--- tests/test_xwakes_impedances_vs_analytic.py
+++ tests/test_xwakes_impedances_vs_analytic.py
@@ -124,3 +124,66 @@ def test_impedances_vs_analytic_resonator_longitudinal():
         Z_from_zeta, Z_analytical, rtol=0, atol=1e-4 * np.max(np.abs(Z_analytical)))
     xo.assert_allclose(
         Z_from_t, Z_analytical, rtol=0, atol=1e-4 * np.max(np.abs(Z_analytical)))
+
+
+@pytest.mark.parametrize('wake_type', ['dipolar'])
+@pytest.mark.parametrize('plane', ['x', 'y'])
+def test_impedances_vs_analytic_thick_res_wall_transverse(wake_type, plane):
+
+    beta0 = 0.1
+
+    wake = xw.WakeThickResistiveWall(
+        kind=f'{wake_type}_{plane}',
+        resistivity=1e-7,
+        radius=1e-2
+        )
+
+    assert len(wake.components) == 1
+    assert wake.components[0].plane == plane
+    assert wake.components[0].source_exponents == {
+        'dipolar_x': (1, 0), 'dipolar_y': (0, 1),
+        'quadrupolar_x': (0, 0), 'quadrupolar_y': (0, 0)}[f'{wake_type}_{plane}']
+    assert wake.components[0].test_exponents == {
+        'dipolar_x': (0, 0), 'dipolar_y': (0, 0),
+        'quadrupolar_x': (1, 0), 'quadrupolar_y': (0, 1)}[f'{wake_type}_{plane}']
+
+    z = np.linspace(-50, 50, 1000000)
+    t = z / beta0 / clight
+
+    w_vs_zeta = wake.components[0].function_vs_zeta(z, beta0=beta0, dzeta=1e-4)
+
+    w_vs_t = wake.components[0].function_vs_t(t, beta0=beta0, dt=1e-4/beta0/clight)
+
+    # Assert that the function is positive at close to zero from the right
+    assert wake.components[0].function_vs_t(1e-10, beta0=beta0, dt=1e-20) > 0
+    assert wake.components[0].function_vs_t(-1e-10, beta0=beta0, dt=1e-20) == 0
+
+    # Zeta has opposite sign compared to t
+    assert wake.components[0].function_vs_zeta(-1e-3, beta0=beta0, dzeta=1e-20) > 0
+    assert wake.components[0].function_vs_zeta(+1e-3, beta0=beta0, dzeta=1e-20) == 0
+
+    omega = np.linspace(-10e9, 10e9, 80)
+
+    Z_from_zeta = omega * (1 + 1j)
+    Z_from_t = omega * (1 + 1j)
+    for ii, oo in enumerate(omega):
+        print(ii, end='\r', flush=True)
+        Z_from_zeta[ii] = 1j/beta0/clight * np.trapezoid(w_vs_zeta
+                    * np.exp(1j * oo * z / beta0 / clight), z)
+        Z_from_t[ii] = 1j * np.trapezoid(
+                    w_vs_t * np.exp(-1j * oo * t), t)
+
+    Z_analytical = wake.components[0].impedance(omega/2/np.pi)
+
+    f_test_sign = 1e9
+    xo.assert_allclose(wake.components[0].impedance(-f_test_sign).real,
+                    -wake.components[0].impedance(f_test_sign).real,
+                    atol=0, rtol=1e-8)
+    xo.assert_allclose(wake.components[0].impedance(-f_test_sign).imag,
+                    wake.components[0].impedance(f_test_sign).imag,
+                    atol=0, rtol=1e-8)
+
+    xo.assert_allclose(
+        Z_from_zeta, Z_analytical, rtol=0, atol=0.1 * np.max(np.abs(Z_analytical)))
+    xo.assert_allclose(
+        Z_from_t, Z_analytical, rtol=0, atol=0.1 * np.max(np.abs(Z_analytical)))
