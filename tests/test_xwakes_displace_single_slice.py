7270c24dc4d20a08e9672ff007c797a13b9ebe2d
giadarol <giovanni.iadarola@cern.ch>
2024-07-11 10:19:48 +0200

    This passes

--- tests/test_xwakes_displace_single_slice.py
+++ tests/test_xwakes_displace_single_slice.py
@@ -57,3 +57,55 @@ def test_longitudinal_wake_kick(test_context):
     # Resonator frequency chosen to have practically constant wake
     xo.assert_allclose(particles.delta[0] - delta_bef[0], scale * w_zero_plus * 3 / 2,  # 1 from particle in front
                         rtol=1e-4, atol=0)                               # 1/2 from particle itself
+
+@for_all_test_contexts(excluding=exclude_contexts)
+def test_constant_wake_kick(test_context):
+
+    p0c = 7000e9
+    h_RF = 600
+    n_slices = 100
+    circumference = 26658.883
+    bucket_length = circumference/h_RF
+    zeta_range = (-0.5*bucket_length, 0.5*bucket_length)
+    dz = (zeta_range[1] - zeta_range[0])/n_slices
+    zeta = np.linspace(zeta_range[0] + dz/2, zeta_range[1]-dz/2,
+                       n_slices)
+
+    i_source = -10
+    i_test = 10
+
+    particles = xt.Particles(
+        mass0=xt.PROTON_MASS_EV,
+        p0c=p0c,
+        zeta=np.array([zeta[i_test], zeta[i_source]]),
+        weight=1e12,
+        _context=test_context
+    )
+
+    px_bef = particles.px.copy()
+    py_bef = particles.py.copy()
+
+    wf = xw.WakeResonator(kind=['constant_x', 'constant_y'],
+                          r=1e8, q=1e7, f_r=1e3)
+    wf.configure_for_tracking(zeta_range=zeta_range, num_slices=n_slices)
+
+    line = xt.Line(elements=[wf],
+                   element_names=['wf'])
+    line.build_tracker()
+    line.track(particles, num_turns=1)
+
+    scale = particles.q0**2 * qe**2 / (particles.p0c[0] * qe) * particles.weight[0]
+
+    assert len(wf.components) == 2
+    comp_x = wf.components[0]
+    comp_y = wf.components[1]
+    assert comp_x.plane == 'x'
+    assert comp_y.plane == 'y'
+    xo.assert_allclose((particles.px - px_bef)[0],
+            scale * comp_x.function_vs_zeta(-particles.zeta[1] +
+                particles.zeta[0], beta0=particles.beta0[0], dzeta=1e-20),
+            rtol=1e-4, atol=0)
+    xo.assert_allclose((particles.py - py_bef)[0],
+            scale *comp_y.function_vs_zeta(-particles.zeta[1] +
+                particles.zeta[0], beta0=particles.beta0[0], dzeta=1e-20),
+            rtol=1e-4, atol=0)
