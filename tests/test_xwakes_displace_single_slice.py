1e00d1dfc3d94167c1d59923b40e8ac326a28560
giadarol <giovanni.iadarola@cern.ch>
2024-07-11 10:27:48 +0200

    Add test

--- tests/test_xwakes_displace_single_slice.py
+++ tests/test_xwakes_displace_single_slice.py
@@ -104,6 +104,10 @@ def test_constant_wake_kick(test_context):
     comp_y = wf.components[1]
     assert comp_x.plane == 'x'
     assert comp_y.plane == 'y'
+    assert comp_x.source_exponents == (0, 0)
+    assert comp_x.test_exponents == (0, 0)
+    assert comp_y.source_exponents == (0, 0)
+    assert comp_y.test_exponents == (0, 0)
     xo.assert_allclose((particles.px - px_bef)[0],
             scale * comp_x.function_vs_zeta(-particles.zeta[1] +
                 particles.zeta[0], beta0=particles.beta0[0], dzeta=1e-20),
@@ -112,3 +116,75 @@ def test_constant_wake_kick(test_context):
             scale *comp_y.function_vs_zeta(-particles.zeta[1] +
                 particles.zeta[0], beta0=particles.beta0[0], dzeta=1e-20),
             rtol=1e-4, atol=0)
+
+
+@for_all_test_contexts(excluding=exclude_contexts)
+def test_dipolar_wake_kick(test_context):
+
+    p0c = 1.2e9
+    h_RF = 600
+    n_slices = 100
+    circumference = 26658.883
+    bucket_length = circumference/h_RF
+    zeta_range = (-0.5*bucket_length, 0.5*bucket_length)
+    dz = (zeta_range[1] - zeta_range[0])/n_slices
+    zeta = np.linspace(zeta_range[0] + dz/2, zeta_range[1]-dz/2,
+                       n_slices)
+
+    i_source = -10
+
+    particles = xt.Particles(
+        mass0=xt.PROTON_MASS_EV,
+        p0c=p0c,
+        zeta=zeta,
+        weight=1e12,
+        _context=test_context
+    )
+
+    displace_x = 2e-3
+    displace_y = 3e-3
+
+    particles.x[i_source] += displace_x
+    particles.y[i_source] += displace_y
+
+    px_bef = particles.px.copy()
+    py_bef = particles.py.copy()
+
+    wf = xw.WakeResonator(kind=['dipolar_x', 'dipolar_y'],
+                          r=1e8, q=1e7, f_r=1e3)
+    wf.configure_for_tracking(zeta_range=zeta_range, num_slices=n_slices)
+
+    line = xt.Line(elements=[wf],
+                   element_names=['wf'])
+    line.build_tracker()
+    line.track(particles, num_turns=1)
+
+    scale = particles.q0**2 * qe**2 / (
+        particles.p0c[0] * particles.beta0[0]* qe) * particles.weight[0]
+
+    assert len(wf.components) == 2
+    comp_x = wf.components[0]
+    comp_y = wf.components[1]
+    assert comp_x.plane == 'x'
+    assert comp_y.plane == 'y'
+    assert comp_x.source_exponents == (1, 0)
+    assert comp_x.test_exponents == (0, 0)
+    assert comp_y.source_exponents == (0, 1)
+    assert comp_y.test_exponents == (0, 0)
+    mask = particles.zeta < particles.zeta[i_source]
+    assert np.allclose(
+        (particles.px - px_bef)[mask]/displace_x,
+        (comp_x.function_vs_zeta(-particles.zeta[i_source] +
+                              particles.zeta,
+                              beta0=particles.beta0[0],
+                              dzeta=1e-20)
+                               * scale)[mask],
+                       rtol=1e-4, atol=0)
+    assert np.allclose(
+        (particles.py - py_bef)[mask]/displace_y,
+        (comp_y.function_vs_zeta(-particles.zeta[i_source] +
+                              particles.zeta,
+                              beta0=particles.beta0[0],
+                              dzeta=1e-20)
+                               * scale)[mask],
+                       rtol=1e-4, atol=0)
