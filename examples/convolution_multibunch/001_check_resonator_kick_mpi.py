cec9b14d8fa2a114f666a91e01f0247f7637b5cc
giadarol <giovanni.iadarola@cern.ch>
2024-08-13 17:36:53 +0200

    a test to be completed

--- examples/convolution_multibunch/001_check_resonator_kick_mpi.py
+++ examples/convolution_multibunch/001_check_resonator_kick_mpi.py
@@ -8,10 +8,14 @@ sigma_zeta = 0.03
 circumference = 1
 filling_scheme=[1, 1, 0, 1]
 
+r=1e8
+q=1e5
+f_r=1e3
+
 kind = ['longitudinal', 'dipolar_x', 'quadrupolar_y']
 wf = xw.WakeResonator(
         kind=kind,
-        r=1e8, q=1e5, f_r=1e3)
+        r=r, q=q, f_r=f_r)
 
 wf.configure_for_tracking(
     zeta_range=(-3*sigma_zeta, 3*sigma_zeta),
@@ -35,7 +39,7 @@ line = xt.Line(elements=elements, element_names=list(elements.keys()))
 line.particle_ref = xt.Particles(p0c=7000e9)
 line.build_tracker()
 
-particles = xp.generate_matched_gaussian_multibunch_beam(
+pars_bunch_gen = dict(
             line=line,
             filling_scheme=filling_scheme,
             num_particles=1_000,
@@ -43,10 +47,27 @@ particles = xp.generate_matched_gaussian_multibunch_beam(
             nemitt_x=2e-6, nemitt_y=2e-6, sigma_z=sigma_zeta,
             bunch_spacing_buckets=bunch_spacing_buckets,
             bucket_length=bucket_length_m,
-            particle_ref=line.particle_ref,
-            #prepare_line_and_particles_for_mpi_wake_sim=True
+            particle_ref=line.particle_ref
 )
 
+particles = xp.generate_matched_gaussian_multibunch_beam(
+    **pars_bunch_gen,
+    #prepare_line_and_particles_for_mpi_wake_sim=True
+)
+particles_all = xp.generate_matched_gaussian_multibunch_beam(**pars_bunch_gen)
+
 line.discard_tracker()
-line.element_names.remove('matrix')
+line.element_names.remove('matrix') # leave only the wake
 line.build_tracker()
+
+wf_all = xw.WakeResonator(
+        kind=kind,
+        r=r, q=q, f_r=f_r)
+wf_all.configure_for_tracking(
+    zeta_range=(-0.85, 3*sigma_zeta),
+    num_slices=1000,
+    )
+wf_all.track(particles_all)
+# line.track(particles)
+
+
