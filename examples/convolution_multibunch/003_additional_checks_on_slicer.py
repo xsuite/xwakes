154190f2ed6c3b485334574d101b2c74a80945cc
giadarol <giovanni.iadarola@cern.ch>
2024-08-13 22:19:31 +0200

    Add zeta_range property

--- examples/convolution_multibunch/003_additional_checks_on_slicer.py
+++ examples/convolution_multibunch/003_additional_checks_on_slicer.py
@@ -1,6 +1,9 @@
 import xfields as xf
 import xtrack as xt
 import numpy as np
+import xobjects as xo
+
+buffer_round_trip = True
 
 slicer = xf.UniformBinSlicer(
     zeta_range=(-1, 1),
@@ -15,15 +18,9 @@ slicer2 = xf.UniformBinSlicer(
     bunch_spacing_zeta=5,
     bunch_numbers=[1, 2])
 
-assert (slicer.filled_slots == np.array([0, 2, 3])).all()
-assert (slicer2.filled_slots == np.array([0, 2, 3])).all()
-assert (slicer.bunch_numbers == np.array([0, 1, 2])).all()
-assert (slicer2.bunch_numbers == np.array([1, 2])).all()
-assert slicer.num_bunches == 3
-assert slicer2.num_bunches == 2
-
-
-
+if buffer_round_trip:
+    slicer = xf.UniformBinSlicer._from_npbuffer(slicer._to_npbuffer())
+    slicer2 = xf.UniformBinSlicer._from_npbuffer(slicer2._to_npbuffer())
 
 particles = xt.Particles(p0c=7000e9,
                          zeta=np.linspace(-20, 20, 1000000))
@@ -31,6 +28,29 @@ particles = xt.Particles(p0c=7000e9,
 slicer.slice(particles)
 
 slicer2.slice(particles)
+
+assert (slicer.filled_slots == np.array([0, 2, 3])).all()
+assert (slicer2.filled_slots == np.array([0, 2, 3])).all()
+assert (slicer.bunch_numbers == np.array([0, 1, 2])).all()
+assert (slicer2.bunch_numbers == np.array([1, 2])).all()
+assert slicer.num_bunches == 3
+assert slicer2.num_bunches == 2
+
+xo.assert_allclose(slicer.zeta_centers,
+    np.array([[ -0.9,  -0.7,  -0.5,  -0.3,  -0.1,   0.1,   0.3,   0.5,   0.7,   0.9],
+              [-10.9, -10.7, -10.5, -10.3, -10.1,  -9.9,  -9.7,  -9.5,  -9.3,  -9.1],
+              [-15.9, -15.7, -15.5, -15.3, -15.1, -14.9, -14.7, -14.5, -14.3, -14.1]]),
+    rtol=0, atol=1e-12)
+
+xo.assert_allclose(slicer2.zeta_centers,
+    np.array([[-10.9, -10.7, -10.5, -10.3, -10.1,  -9.9,  -9.7,  -9.5,  -9.3,  -9.1],
+              [-15.9, -15.7, -15.5, -15.3, -15.1, -14.9, -14.7, -14.5, -14.3, -14.1]]),
+    rtol=0, atol=1e-12)
+
+xo.assert_allclose(slicer.zeta_range[0], -1, rtol=0, atol=1e-12)
+xo.assert_allclose(slicer2.zeta_range[0], -1, rtol=0, atol=1e-12)
+xo.assert_allclose(slicer.zeta_range[1], 1, rtol=0, atol=1e-12)
+xo.assert_allclose(slicer2.zeta_range[1], 1, rtol=0, atol=1e-12)
 
 import matplotlib.pyplot as plt
 plt.close('all')
