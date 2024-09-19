a0f874a98870b07aa1a547df1b149c7d0ee93879
lgiacome <lorenzo.giacomel@cern.ch>
2024-09-19 14:08:39 +0200

    add test wake in front

--- tests/test_xwakes_wake_kicks.py
+++ tests/test_xwakes_wake_kicks.py
@@ -754,3 +754,109 @@ def test_wake_kick_indirect_space_charge(test_context, kind):
 
         xo.assert_allclose(getattr(particles, dict_p_bef[kk][0]) - dict_p_bef[kk][1],
                             expected, rtol=1e-4, atol=1e-20)
+
+@for_all_test_contexts(excluding=exclude_contexts)
+@pytest.mark.parametrize('kind', [
+    ['longitudinal'],
+    ['constant_x', 'constant_y'],
+    ['dipolar_x','dipolar_y'],
+    ['dipolar_xy','dipolar_yx'],
+    ['quadrupolar_x','quadrupolar_y'],
+    ['quadrupolar_xy','quadrupolar_yx'],
+    ])
+def test_wake_in_front(test_context, kind):
+    # we test the wake kick when the wake is in front of the source particle,
+    # which is achieved by flipping the wake function of a resonator
+    h_bunch = 600
+    circumference=26658.883
+    bucket_length = circumference/h_bunch/10
+    zeta_range = (-0.5*bucket_length, 0.5*bucket_length)
+    num_slices = 100
+
+    wf_pos = xw.WakeResonator(
+        kind=kind,
+        r=1e8, q=1e5, f_r=1e3)
+
+    wf = xw.WakeResonator(
+        kind=kind,
+        r=1e8, q=1e5, f_r=1e3)
+
+    for icomp, component in enumerate(wf.components):
+        comp_pos = wf_pos.components[icomp]
+        component.function_vs_zeta = (
+            lambda zeta, dzeta, beta0: comp_pos.function_vs_zeta(zeta=-zeta,
+                                                                 beta0=beta0,
+                                                                 dzeta=dzeta))
+
+
+    wf.configure_for_tracking(
+        zeta_range=zeta_range,
+        num_slices=num_slices,
+        bunch_spacing_zeta=circumference/h_bunch,
+        circumference=circumference
+        )
+    p0c = 7e12
+
+    line = xt.Line(elements=[wf])
+    line.build_tracker()
+    line.particle_ref=xp.Particles(p0c=p0c)
+
+    zeta_parts = wf.slicer.zeta_centers.flatten()
+
+    particles = xp.Particles(p0c=p0c,
+                             zeta=zeta_parts,
+                             weight=1e12,
+                             _context=test_context)
+
+    i_source_x = -2
+    i_source_y = -4
+
+    displace_x = 2e-3
+    displace_y = 3e-3
+
+    # we displace before a certain particle index so that we check also that
+    # in the dipolar and quadrupolar cases there is no wake in behind (opposite of
+    # wake in front)
+    particles.x[:i_source_x] += displace_x
+    particles.y[:i_source_y] += displace_y
+
+    dict_p_bef = {}
+
+    for kk in kind:
+        if kk == 'longitudinal':
+            dict_p_bef[kk] = ('delta', particles.delta.copy())
+        elif kk.split('_')[1] == 'x' or kk.split('_')[1] == 'xy':
+            dict_p_bef[kk] = ('px', particles.px.copy())
+        elif kk.split('_')[1] == 'y' or kk.split('_')[1] == 'yx':
+            dict_p_bef[kk] = ('py', particles.py.copy())
+        else:
+            raise ValueError('Invalid kind')
+
+    line.track(particles)
+
+    assert len(wf.components) == len(kind)
+
+    for comp, kk in zip(wf.components, kind):
+        if comp.plane == 'z':
+            scale = -particles.q0**2 * qe**2 / (
+                particles.p0c[0] * particles.beta0[0]* qe) * particles.weight[0]
+        else:
+            scale = particles.q0**2 * qe**2 / (
+                particles.p0c[0] * particles.beta0[0]* qe) * particles.weight[0]
+        assert comp.plane == kind_to_parameters[kk]['plane']
+        assert comp.source_exponents == kind_to_parameters[kk]['source_exponents']
+        assert comp.test_exponents == kind_to_parameters[kk]['test_exponents']
+
+        expected = np.zeros_like(particles.zeta)
+
+        for i_test, z_test in enumerate(particles.zeta):
+            expected[i_test] += (particles.x[i_test]**comp.test_exponents[0] *
+                                 particles.y[i_test]**comp.test_exponents[1] *
+                                 np.dot(particles.x**comp.source_exponents[0] *
+                                        particles.y**comp.source_exponents[1],
+                                        comp.function_vs_zeta(z_test - particles.zeta,
+                                                              beta0=particles.beta0[0],
+                                                              dzeta=1e-12)) * scale)
+
+        xo.assert_allclose(getattr(particles, dict_p_bef[kk][0]) - dict_p_bef[kk][1],
+                            expected, rtol=1e-4, atol=1e-20)
