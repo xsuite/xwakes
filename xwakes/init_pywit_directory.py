df77f8a97708448e7d93f6dfff41ad69cde448f8
Dora Gibellieri <dora.gibellieri@cern.ch>
2024-11-05 10:38:36 +0000

    break if folder already exists

--- xwakes/init_pywit_directory.py
+++ xwakes/init_pywit_directory.py
@@ -6,7 +6,7 @@ def initialize_pywit_directory() -> None:
     home_path = pathlib.Path.home()
     paths = [home_path.joinpath('pywit').joinpath(ext) for ext in ('config', 'IW2D/bin', 'IW2D/projects')]
     for path in paths:
-        os.makedirs(path, exist_ok=True)
+        os.makedirs(path)
     with open(pathlib.Path(paths[0]).joinpath('iw2d_settings.yaml'), 'w') as file:
         file.write(f'binary_directory: {paths[1]}\n'
                    f'project_directory: {paths[2]}')
